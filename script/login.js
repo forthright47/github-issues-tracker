6ce5eec5d84a1dd37c52201fa18a7364301e0d96
forthright47 <k.tuhinjaman@gmail.com>
2026-03-07 14:38:29 +0600

    implement login functionalities and navigation to dashboard

--- script/login.js
+++ script/login.js
@@ -1,1 +1,20 @@
-console.log("Connected successfully");
+document.getElementById("login-btn").addEventListener("click", function(){
+    // console.log("Login button clicked");
+    const usernameInput = document.getElementById("input-username");
+    const userName = usernameInput.value;
+    console.log(userName);
+
+    const passwordInput = document.getElementById("input-password");
+    const password = passwordInput.value;
+    console.log(password);
+
+    if(userName == "admin" && password == "admin123"){
+        alert("Login successfully");
+
+        window.location.assign("/dashboard.html");
+    }
+    else{
+        alert("Invalid username or password");
+        return;
+    }
+})
